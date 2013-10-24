b960c439c14a0ea966c61d7d255cd4eaec79dd71
LindsayL <livojevi@ualberta.ca>
2013-10-24 17:37:40 -0600

    Put both test cases for use case 3 in here and changed the name so that it would potentially compile

---
+++ TestCases/EditStoryActivityTestCases.java
@@ -0,0 +1,43 @@
+/* Covers test cases for use cases:
+ * 3
+ * 
+ * http://stackoverflow.com/questions/10845937/how-to-do-junit-testing-in-android
+ * Referenced Oct 23, 2013 for general format of Test class
+ * 
+ */
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+
+public class EditStoryActivityTestCases extends ActivityInstrumentationTestCase2<EditStoryActivity> {
+	//declare activity and widgets
+	private EditStoryActivity myEditStoryActivity;
+	private TextView myView;
+	
+	//Constructor
+	public EditStoryActivityTestCases(){
+		super("ualberta.g12.adventurecreator.EditStoryActivity", EditStoryActivity.class);
+	}	
+	
+	//initializes things you want to be set for all tests (not necessary)
+	@Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        mActivity = this.getActivity();
+		Button mbutton = (Button) findViewById(R.id.saveStory)
+    }
+
+	/* Test Cases - all start with "test" */
+	
+	//Use Case 3, test 1/2
+	public void testPublishStory {
+		addStory();
+		assert click(mbutton);
+	}
+	
+	//Use Case 3, test 2/2
+	public void testPublishStory {
+		assert click(mbutton);
+	}
+
+}
