e61630838a51e3af3b5c8b28b22b9d82d8860e0a
kclau301 <kclau@ualberta.ca>
2013-10-24 14:47:41 -0600

    new file:   TestCases/Testcase1.1.java 	new file:   TestCases/Testcase1.2.java 	new file:   TestCases/Testcase2.1.java 	new file:   TestCases/Testcase2.2.java 	new file:   TestCases/Testcase2.3.java 	new file:   TestCases/Testcase3.1.java 	new file:   TestCases/Testcase3.2.java

---
+++ TestCases/Testcase2.3.java
@@ -0,0 +1,32 @@
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+
+//http://stackoverflow.com/questions/10845937/how-to-do-junit-testing-in-android
+//Referenced Oct 23, 2013 
+public class ChooseStoryActivityTestCases extends ActivityInstrumentationTestCase2<ChooseStoryActivity> {
+	//declare activity and widgets
+	private ChooseStoryActivity myChooseStoryActivity;
+	private TextView myView;
+	
+	//Constructor
+	public ChooseStoryActivityTestCases(){
+		super("ualberta.g12.adventurecreator.ChooseStoryActivity", ChooseStoryActivity.class);
+	}	
+	
+	//initializes things you want to be set for all tests
+	//(not necessary)
+	@Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        mActivity = this.getActivity();
+        mView = (TextView) mActivity.findViewById(ualberta.g12.adventurecreator.ChooseStoryActivity.R.id.textviewID);
+    }
+
+	/* Test Cases - all start with "test" */
+	
+	public void testBrowseEmptyStoryList {
+		String Story = myView.getText();
+		assertEquals(Story, null);
+	}
+
+}
---
+++ TestCases/Testcase3.1.java
@@ -0,0 +1,32 @@
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
+
+//http://stackoverflow.com/questions/10845937/how-to-do-junit-testing-in-android
+//Referenced Oct 23, 2013 
+public class EditStoryActivityTestCases extends ActivityInstrumentationTestCase2<EditStoryActivity> {
+	//declare activity and widgets
+	private EditStoryActivity myEditStoryActivity;
+	private TextView myView;
+	
+	//Constructor
+	public EditStoryActivityTestCases(){
+		super("ualberta.g12.adventurecreator.EditStoryActivity", EditStoryActivity.class);
+	}	
+	
+	//initializes things you want to be set for all tests
+	//(not necessary)
+	@Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        mActivity = this.getActivity();
+		Button mbutton = (Button) findViewById(R.id.saveStory)
+    }
+
+	/* Test Cases - all start with "test" */
+	
+	public void testPublishStory {
+		addStory();
+		assert click(mbutton);
+	}
+
+}
