e450653e3a91c1ed6e9199969497fdbc0fd94486
LindsayL <livojevi@ualberta.ca>
2013-10-24 20:21:22 -0600

    part of use case 14's test case implemented

--- TestCases/FragmentActivityTestCases.java
+++ TestCases/FragmentActivityTestCases.java
@@ -1,5 +1,6 @@
+package g12.projecttestcases;
 /* Covers test cases for use cases:
- * 1
+ * 1, 14
  * 
  * http://stackoverflow.com/questions/10845937/how-to-do-junit-testing-in-android
  * Referenced Oct 23, 2013 for general format of Test class
@@ -7,39 +8,71 @@
  */
 
 
+import ca.ualberta.cs.livojevi_notes.EntryListActivity;
+import ca.ualberta.cs.livojevi_notes.ViewEntryActivity;
+import android.content.Intent;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class FragmentActivityTestCases extends ActivityInstrumentationTestCase2<FragmentActivity> {
 	//declare activity and widgets
 	private FragmentActivity myFragmentActivity;
 	private TextView myView;
-	
+	private Button annotateButton;
+
 	//Constructor
 	public FragmentActivityTestCases(){
 		super("ualberta.g12.adventurecreator.FragmentActivity", FragmentActivity.class);
 	}	
-	
+
 	//initializes things you want to be set for all tests (not necessary)
 	@Override
-    protected void setUp() throws Exception {
-        super.setUp();
-        mActivity = this.getActivity();
-        mView = (TextView) mActivity.findViewById(ualberta.g12.adventurecreator.FragemntActivity.R.id.textviewID);
-    }
+	protected void setUp() throws Exception {
+		super.setUp();
+
+	}
 
 	/* Test Cases - all start with "test" */
-	
+
 	// Use Case 1, test 1/2
-	public void testReadStoryFragments {
+	public void testReadStoryFragments() {
+		//Start Activity
+		myFragmentActivity = this.getActivity();
+		mView = (TextView) myFragmentActivity.findViewById(ualberta.g12.adventurecreator.FragemntActivity.R.id.textviewID);
+
+
 		String fragment = myView.getText();
 		assertNotEquals(fragment, null);
 	}
-	
+
 	// Use Case 2, test 2/2
-	public void testReadNoFragments {
+	public void testReadNoFragments() {
+		//Start Activity
+		myFragmentActivity = this.getActivity();
+		mView = (TextView) myFragmentActivity.findViewById(ualberta.g12.adventurecreator.FragemntActivity.R.id.textviewID);
+
 		String fragment = myView.getText();
 		assertEquals(fragment, null);
+	}
+
+	// Use Case 14, test 1/1
+	public void testannotateFragment() {
+		Fragment someFragment = new Fragment();
+		
+		//set up intent to pass info to activity
+		Intent FragmentIntent = new Intent(ChooseStoryActivity.this, FragmentActivity.class);
+		FragmentIntent.putExtra("FragmentPass", someFragment);
+		
+		//pass intent to activity
+		setActivityIntent(FragmentIntent);
+		
+		//Start Activity
+		myFragmentActivity = this.getActivity();
+		annotateButton = (Button) myFragmentActivity.findViewById(ualberta.g12.adventurecreator.FragemntActivity.R.id.annotateButtonID);
+
+		//click annotate button and confirm there was a listener
+		assertTrue(annotateButton.performClick());
 	}
 
 }
