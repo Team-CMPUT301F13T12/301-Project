07a379906943401aaf8ea4c9668a173a9eacc1e1
LindsayL <livojevi@ualberta.ca>
2013-10-24 19:22:40 -0600

    added test case for use case 16, might give some inspiration for other cases?

--- TestCases/ChooseStoryActivityTestCases.java
+++ TestCases/ChooseStoryActivityTestCases.java
@@ -1,50 +1,94 @@
 /* Covers test cases for use cases:
- * 2
+ * 2, 16
  * 
  * http://stackoverflow.com/questions/10845937/how-to-do-junit-testing-in-android
  * Referenced Oct 23, 2013 for general format of Test class
  * 
  */
 
+import ca.ualberta.cs.livojevi_notes.R;
+import ca.ualberta.cs.livojevi_notes.TitleAndDescriptionAdapter;
 import android.test.ActivityInstrumentationTestCase2;
+import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 
 public class ChooseStoryActivityTestCases extends ActivityInstrumentationTestCase2<ChooseStoryActivity> {
 	//declare activity and widgets
 	private ChooseStoryActivity myChooseStoryActivity;
 	private TextView myView;
-	
+	private ListView availableStoryList;
+	private PopupMenu storyListPopupMenu;
+
 	//Constructor
 	public ChooseStoryActivityTestCases(){
 		super("ualberta.g12.adventurecreator.ChooseStoryActivity", ChooseStoryActivity.class);
 	}	
-	
+
 	//initializes things you want to be set for all tests (not necessary)
 	@Override
-    protected void setUp() throws Exception {
-        super.setUp();
-        mActivity = this.getActivity();
-        mView = (TextView) mActivity.findViewById(ualberta.g12.adventurecreator.ChooseStoryActivity.R.id.textviewID);
-    }
+	protected void setUp() throws Exception {
+		super.setUp();
+		myActivity = this.getActivity();
+		myView = (TextView) mActivity.findViewById(ualberta.g12.adventurecreator.ChooseStoryActivity.R.id.textviewID);
+		availableStoryList = (ListView) mActivity.findViewById(ualberta.g12.adventurecreator.ChooseStoryActivity.R.id.storyListID);
+		storyListPopupMenu = (PopupMenu) mActivity.findViewById(ualberta.g12.adventurecreator.ChooseStoryActivity.R.id.storyListPoupMenuID);
+	}
 
 	/* Test Cases - all start with "test" */
-	
+
 	//Use Case 2, test 1/3
-	public void testBrowseStoryList {
+	public void testBrowseStoryList() {
 		addMultiStory();
+
+		//starts the activity
+		getActivity();
+
 		assertTrue(scrollable(mView.getText()));
 	}
-	
+
 	//Use Case 2, test 2/3
-	public void testBrowseNoScrollList {
+	public void testBrowseNoScrollList() {
 		addOneStory();
+
+		//starts the activity
+		getActivity();
+
 		assertFalse(scrollable(mView.getText()));
 	}
-	
+
 	//Use Case 2, test 3/3
-	public void testBrowseEmptyStoryList {
+	public void testBrowseEmptyStoryList() {
+		//starts the activity
+		getActivity();
 		String Story = myView.getText();
 		assertEquals(Story, null);
+	}
+
+	//Use Case 16, test 1/1
+	public void testDowloadOtherUserStory() {
+		Story otherUserStory = new Story();
+		otherUserStory.setAuthor("OtherUser");
+
+		//uploads/publishes story
+		addStory(otherUserStory);
+
+		//starts the activity (which will automatically find all 
+		//available stories and load them into the availableStoryList)
+		getActivity();
+
+		//selects first (and only) story
+		availableStoryList.setSelection(0);
+		
+		//causes popupMenu to open for selection
+		availableStoryList.performLongClick();
+
+		//gets download option if available
+		MenuItem downloadOption = storyListPopupMenu.getMenu().getItem(2);
+
+		//asserts user can Download the story
+		assertTrue(downloadOption.getTitle().equals("Download"));
 	}
 
 }
