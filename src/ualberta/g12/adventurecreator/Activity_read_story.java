6c4227a29fcfdd21319022d95c7e54b22a0d7557
kclau301 <kclau@ualberta.ca>
2013-10-30 16:59:10 -0600

    modified:   AndroidManifest.xml 	new file:   libs/android-support-v4.jar 	new file:   res/drawable-hdpi/test.png 	new file:   res/drawable-mdpi/test.png 	new file:   res/drawable-xhdpi/test.png 	new file:   res/drawable-xxhdpi/test.png 	new file:   res/layout/activity_activity_read_story.xml 	new file:   res/menu/activity_read_story.xml 	modified:   res/values/strings.xml 	new file:   src/ualberta/g12/adventurecreator/Activity_read_story.java 	new file:   test-web.png

---
+++ src/ualberta/g12/adventurecreator/Activity_read_story.java
@@ -0,0 +1,23 @@
+
+package ualberta.g12.adventurecreator;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Activity_read_story extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity_read_story);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_read_story, menu);
+        return true;
+    }
+
+}
