e77a875b77767afde5f4bd8d9b170f7aaa452ec6
Chris <tophernuts@gmail.com>
2013-11-01 20:08:07 -0600

    Cleaned up Fragments

--- src/ualberta/g12/adventurecreator/Fragment.java
+++ src/ualberta/g12/adventurecreator/Fragment.java
@@ -1,3 +1,4 @@
+
 package ualberta.g12.adventurecreator;
 
 import android.graphics.drawable.Drawable;
@@ -6,103 +7,117 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class Fragment {
-	private String title;
-	private String bodyText;
-	private List<Drawable> illustrations;
-	//private List<Sound> sounds;
-	//private List<Video> videos;
-	private List<Choice> choices;
-	private List<String> displayOrder; //Contains one character representations of each type to display 
-	//True if at least one page references it, can be used a flag for isolated pages
-	//will have to be controlled from the story object
-	private boolean isLinkedTo; 
-	//private Annotation annotations;
-	
-	public Fragment() {
-		this.title = "Choose a Title";
-		this.bodyText = "Story body here.";
-		this.choices = new LinkedList<Choice>();
-		this.choices.add(new Choice()); //Add one default choice to start
-		//true because it is only possible to create a new page from a parent page
-		//or if it is the first page in a story
-		isLinkedTo = true; 
-	}
-	
-	public String getTitle() {
-		return this.title;
-	}
+    private String title;
+    private String bodyText;
+    private List<Drawable> illustrations;
+    // private List<Sound> sounds;
+    // private List<Video> videos;
+    private List<Choice> choices;
+    private List<String> displayOrder; // Contains one character representations
+                                       // of each type to display
+    // True if at least one page references it, can be used a flag for isolated
+    // pages
+    // will have to be controlled from the story object
+    private boolean isLinkedTo;
 
-	public void setTitle(String newTitle) {
-		this.title = newTitle;
-	}
+    // private Annotation annotations;
 
-	public String getBodyText() {
-		return bodyText;
-	}
+    public Fragment() {
+        /*
+         * Should this be done here? I feel that this should be moved to the
+         * view that displays a new fragment as the default text or hint.
+         * - Chris
+         */
+        this("Choose a Title", "Story body here.");
+    }
 
-	public void setBodyText(String bodyText) {
-		this.bodyText = bodyText;
-	}
+    public Fragment(String title, String body) {
+        setTitle(title);
+        setBodyText(body);
+        initChoices();
+        isLinkedTo = true;
+    }
 
-	public List<Drawable> getIllustrations() {
-		return illustrations;
-	}
+    /** Each fragment should have one default choice */
+    private void initChoices() {
+        this.choices.add(new Choice());
+    }
 
-	public void setIllustrations(List<Drawable> illustrations) {
-		this.illustrations = illustrations;
-	}
+    public String getTitle() {
+        return this.title;
+    }
 
-//	public List<Sound> getSounds() {
-//		return sounds;
-//	}
-//
-//	public void setSounds(List<Sound> sounds) {
-//		this.sounds = sounds;
-//	}
-//
-//	public List<Video> getVideos() {
-//		return videos;
-//	}
-//
-//	public void setVideos(List<Video> videos) {
-//		this.videos = videos;
-//	}
+    public void setTitle(String newTitle) {
+        this.title = newTitle;
+    }
 
-	public List<Choice> getChoices() {
-		return choices;
-	}
+    public String getBodyText() {
+        return bodyText;
+    }
 
-	public void setChoices(List<Choice> newChoices) {
-		this.choices = newChoices;
-	}
-	
-	public void removeChoice(Choice oldChoice) {
-		this.choices.remove(oldChoice);
-	}
+    public void setBodyText(String bodyText) {
+        this.bodyText = bodyText;
+    }
 
-	public List<String> getDisplayOrder() {
-		return displayOrder;
-	}
+    public List<Drawable> getIllustrations() {
+        return illustrations;
+    }
 
-	public void setDisplayOrder(List<String> displayOrder) {
-	    // Does this belong here or in the StoryController
-	    this.displayOrder = displayOrder;
-	}
+    public void setIllustrations(List<Drawable> illustrations) {
+        this.illustrations = illustrations;
+    }
 
-	public boolean isLinkedTo() {
-		return isLinkedTo;
-	}
+    // public List<Sound> getSounds() {
+    // return sounds;
+    // }
+    //
+    // public void setSounds(List<Sound> sounds) {
+    // this.sounds = sounds;
+    // }
+    //
+    // public List<Video> getVideos() {
+    // return videos;
+    // }
+    //
+    // public void setVideos(List<Video> videos) {
+    // this.videos = videos;
+    // }
 
-	public void setLinkedTo(boolean isLinkedTo) {
-		this.isLinkedTo = isLinkedTo;
-	}
+    public List<Choice> getChoices() {
+        return choices;
+    }
 
-//	public Annotation getAnnotations() {
-//		return annotations;
-//	}
-//
-//	public void setAnnotations(Annotation annotations) {
-//		this.annotations = annotations;
-//	}
+    public void setChoices(List<Choice> newChoices) {
+        this.choices = newChoices;
+    }
+
+    public void removeChoice(Choice oldChoice) {
+        this.choices.remove(oldChoice);
+    }
+
+    public List<String> getDisplayOrder() {
+        return displayOrder;
+    }
+
+    public void setDisplayOrder(List<String> displayOrder) {
+        // Does this belong here or in the StoryController
+        this.displayOrder = displayOrder;
+    }
+
+    public boolean isLinkedTo() {
+        return isLinkedTo;
+    }
+
+    public void setLinkedTo(boolean isLinkedTo) {
+        this.isLinkedTo = isLinkedTo;
+    }
+
+    // public Annotation getAnnotations() {
+    // return annotations;
+    // }
+    //
+    // public void setAnnotations(Annotation annotations) {
+    // this.annotations = annotations;
+    // }
 
 }
