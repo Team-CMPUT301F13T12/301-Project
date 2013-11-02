c62e065d8c3e3542a4b1b8972e6ecf3e406a81b0
Chris <tophernuts@gmail.com>
2013-11-02 15:11:33 -0600

    Updated FragmentController to not have its own Fragment.
    Doesn't make sense to pass the fragment controller a Fragment in it's constructor since we will be changing this all of the time.
    Instead we now have to pass a Fragment object into all of the methods which will then modify them

--- src/ualberta/g12/adventurecreator/FController.java
+++ src/ualberta/g12/adventurecreator/FController.java
@@ -11,28 +11,28 @@ public interface FController {
      * 
      * @param t the new title of the fragment
      */
-    public void editTitle(String t);
+    public void editTitle(Fragment f,String t);
 
     /**
      * Edits the body text of the fragment
      * 
      * @param t the new body text of the fragment
      */
-    public void editBodyText(String b);
+    public void editBodyText(Fragment f, String b);
 
     /**
      * Adds illustration to a fragment
      * 
      * @param i the illustration to add to the fragment
      */
-    public void addIllustration(Drawable i);
+    public void addIllustration(Fragment f, Drawable i);
 
     /**
      * Adds a choice to the list of choices that a fragment has
      * 
      * @param c the choice to add to the list
      */
-    public void addChoice(Choice c);
+    public void addChoice(Fragment f, Choice c);
 
     /**
      * Removes a choice from the list of choices a fragment has
@@ -40,15 +40,15 @@ public interface FController {
      * @param c the choice to remove
      * @return true is choice was removed else false
      */
-    public boolean deleteChoice(Choice c);
+    public boolean deleteChoice(Fragment f, Choice c);
     
     // TODO: Implement annotations
-    // public void addAnnotation(Annotation a);
+    // public void addAnnotation(Fragment f, Annotation a);
 
-    // Should these all be replaced by an addMedia(Media m) ?
+    // Should these all be replaced by an addMedia(Fragment f, Media m) ?
     // TODO: Implement adding media
-    // public void addPicture(Picture p);
-    // public void addSound(Sound s);
-    // public void addVideo(Video v);
+    // public void addPicture(Fragment f, Picture p);
+    // public void addSound(Fragment f, Sound s);
+    // public void addVideo(Fragment f, Video v);
 
 }
--- src/ualberta/g12/adventurecreator/FragmentController.java
+++ src/ualberta/g12/adventurecreator/FragmentController.java
@@ -1,42 +1,35 @@
+
 package ualberta.g12.adventurecreator;
 
 import android.graphics.drawable.Drawable;
 
-import java.util.List;
+public class FragmentController implements FController {
 
-public class FragmentController implements FController{
-
-    private Fragment f = null;
-    
-    public FragmentController(Fragment f){
-        this.f =f;
-    }
-    
     @Override
-    public void editBodyText(String b){
+    public void editBodyText(Fragment f, String b) {
         f.setBodyText(b);
     }
-    
+
     @Override
-    public void editTitle(String t) {
+    public void editTitle(Fragment f, String t) {
         f.setTitle(t);
     }
-    
+
     @Override
-    public void addIllustration(Drawable i){
+    public void addIllustration(Fragment f, Drawable i) {
         // TODO: Do something with return value
         f.addIllustration(i);
     }
 
     @Override
-    public void addChoice(Choice c) {
+    public void addChoice(Fragment f, Choice c) {
         // TODO: Do something with the return value
         f.addChoice(c);
     }
 
     @Override
-    public boolean deleteChoice(Choice c) {
+    public boolean deleteChoice(Fragment f, Choice c) {
         return f.removeChoice(c);
     }
-	
+
 }
