57a7952c17bc03ce8168c424cbd2a50154fd2dfa
Chris <tophernuts@gmail.com>
2013-11-03 13:54:00 -0700

    Added working ListView of Stories

--- src/ualberta/g12/adventurecreator/MainActivity.java
+++ src/ualberta/g12/adventurecreator/MainActivity.java
@@ -1,37 +1,45 @@
+
 package ualberta.g12.adventurecreator;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 import java.util.List;
 
-public class MainActivity extends Activity implements LView<StoryList>{
+public class MainActivity extends Activity implements LView<StoryList> {
 
     private List<Story> stories;
     private StoryList storyList;
-    
     private static final boolean DEBUG_LOG = true;
     private static final String TAG = "MainActivity";
-    
+    private ListView listView;
+    private StoryListArrayAdapter adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        // TODO: Get our storyList instance from the application
-        
-        // TODO: Load our local stories from the StoryList Model
-        // Something like stories = storyList.getAllStories()
-        
-        // TODO: Set up ListView Stuff
-        
-        // TODO: Set up listeners on items 
-        
-    }
 
+        // TODO: Get our storyList instance from the application
+        storyList = AdventureCreatorApplication.getStoryList();
+        // TODO: Load our local stories from the StoryList Model
+        stories = storyList.getAllStories();
+
+        if (DEBUG_LOG)
+            Log.d(TAG, String.format("Number of stories is: %d", stories.size()));
+
+        // TODO: Set up ListView Stuff
+        adapter = new StoryListArrayAdapter(this, R.layout.listview_story_list, stories);
+        listView = (ListView) findViewById(R.id.listview);
+        listView.setAdapter(adapter);
+        // TODO: Set up listeners on items
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -40,28 +48,26 @@ public class MainActivity extends Activity implements LView<StoryList>{
         return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // TODO Auto-generated method stub
+        switch (item.getItemId()) {
+            case R.id.add_story:
+                // TODO: Launch the EditStoryActivity with the id of NEW_STORY
+                startActivity(new Intent(this, CreateStoryActivity.class));
+                break;
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// TODO Auto-generated method stub
-		switch(item.getItemId()){
-		case R.id.add_story:
-		    // TODO: Launch the EditStoryActivity with the id of NEW_STORY
-		    startActivity(new Intent(this, CreateStoryActivity.class));
-			break;
-			
-		case R.id.settings:
-			break;
-		}
-		return super.onOptionsItemSelected(item);
-	}
-
+            case R.id.settings:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     public void update(StoryList model) {
         // TODO: Reload our stories from StoryList Model
-        
+
         // TODO: Notify Our ListView that our array has changed
-        
+
     }
 }
---
+++ src/ualberta/g12/adventurecreator/StoryListArrayAdapter.java
@@ -0,0 +1,60 @@
+
+package ualberta.g12.adventurecreator;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class StoryListArrayAdapter extends ArrayAdapter<Story> {
+    private final Context context;
+
+    private static final boolean DEBUG_LOG = true;
+    private static final String TAG = "StoryListArrayAdapter";
+
+    public StoryListArrayAdapter(Context context, int id, List<Story> list) {
+        super(context, id, list);
+        this.context = context;
+        if (DEBUG_LOG)
+            Log.d(TAG, "Creating a story list array adapter k?");
+    }
+
+    private class ViewHolder {
+        TextView title;
+        TextView author;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = null;
+        Story story = getItem(position);
+
+        LayoutInflater mInflater = (LayoutInflater) context
+                .getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.listview_story_list, null);
+            holder = new ViewHolder();
+            holder.title = (TextView) convertView.findViewById(R.id.list_story_title);
+            holder.author = (TextView) convertView.findViewById(R.id.list_story_author);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        if (DEBUG_LOG) {
+            Log.d(TAG, String.format("ListViewing story with title: %s", story.getStoryTitle()));
+            Log.d(TAG, String.format("ListViewing story with author: %s", story.getAuthor()));
+        }
+        holder.title.setText(story.getStoryTitle());
+        holder.author.setText(story.getAuthor());
+
+        return convertView;
+    }
+}
