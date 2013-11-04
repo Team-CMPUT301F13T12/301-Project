2586bb8576fe73fbf79dcfa31b9e8f4ca1758a4b
Chris <tophernuts@gmail.com>
2013-11-04 01:10:05 -0700

    Added methods to update story author and title

--- src/ualberta/g12/adventurecreator/StoryController.java
+++ src/ualberta/g12/adventurecreator/StoryController.java
@@ -3,6 +3,21 @@ package ualberta.g12.adventurecreator;
 
 public class StoryController implements SController {
 
+    
+    /**
+     * @param newTitle the new title of story s
+     * @param s the story to change the title of*/
+    public void setTitle(String newTitle, Story s){
+        s.setStoryTitle(newTitle);
+    }
+    
+    /**
+     * @param newAuthor the new author of story s
+     * @param s the story to change the author of*/
+    public void setAuthor(String newAuthor, Story s){
+        s.setAuthor(newAuthor);
+    }
+    
     @Override
     public void addFragment(Story s, Fragment f) {
         s.addFragment(f);
