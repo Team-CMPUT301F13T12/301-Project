f7254694af0de1651f8a75d69cd5b993434dfcd3
vzphung <vzphung@ualberta.ca>
2013-11-03 21:06:43 -0700

    test case for story list controller

---
+++ TestCases/storyListController.java
@@ -0,0 +1,54 @@
+package g12.projecttestcases;
+
+
+import ca.ualberta.cs.livojevi_notes.EntryListActivity;
+import ca.ualberta.cs.livojevi_notes.ViewEntryActivity;
+import android.content.Intent;
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class storyListControllerTestCases extends ActivityInstrumentationTestCase2<StoryManager> {
+    
+    private StoryListController slc;
+    private StoryList sl;
+    private List<Story> stories;
+
+    public StoryManagerTestCase(){
+        super("ualberta.g12.adventurecreator.StoryListController", StoryListController.class);
+    }
+
+
+    // test adding a story 
+    public void testAddStory(){
+        
+        // add a story to our storyList
+        sl = new StoryList();
+        slc = new StoryListController(sl);
+        story s = new story("Book","Dan Dude");
+        slc.addStory(s);
+        int numBooks = slc.getSize();
+        
+        assert(numBooks != 0);
+        
+    }
+    
+      //tests deleting a story
+      public void testDeleteStory(){
+        
+        // add a story to our storyList
+        sl = new StoryList();
+        slc = new StoryListController(sl);
+        story s = new story("Book","Dan Dude");
+        slc.addStory(s);
+        slc.deleteStory(s);
+        int numBooks = slc.getSize();
+        
+        assert(numBooks == 0);
+        
+    }
+    
+
+    
+    
+}
