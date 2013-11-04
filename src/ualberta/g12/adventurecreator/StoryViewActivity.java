c88e0c6080fbb5bcabf81422376f011eea9266db
Chris <tophernuts@gmail.com>
2013-11-03 23:10:15 -0700

    Removed Menu from StoryViewActivity

--- src/ualberta/g12/adventurecreator/StoryViewActivity.java
+++ src/ualberta/g12/adventurecreator/StoryViewActivity.java
@@ -15,11 +15,4 @@ public class StoryViewActivity extends Activity {
         setContentView(R.layout.activity_story_view);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.story_view, menu);
-        return true;
-    }
-
 }
