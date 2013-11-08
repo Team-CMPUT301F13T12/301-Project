9281afadcfecf6bb157d45a4c8250bfd128f821c
vzphung <vzphung@ualberta.ca>
2013-11-08 10:11:27 -0700

    Updated StoryControllerTests

--- test-test/src/ualberta/g12/adventurecreator/test/StoryControllerTest.java
+++ test-test/src/ualberta/g12/adventurecreator/test/StoryControllerTest.java
@@ -61,5 +61,41 @@ public class StoryControllerTest extends TestCase {
     	assert(s.getFragments().size() == 0);
     }
 
-
+    /**
+     * Tests getting fragments from a fragment
+     */
+    public void testGetAllFragments() {
+    	Story s = new Story();
+    	Fragment f = new Fragment();
+    	sc.addFragment(s, f);
+    	assert(s.getFragments().size() == 1);
+    	assert(sc.getFragments(s).size() == 1);
+    }
+    
+    /**
+     * Tests getting a fragment at a certain position
+     */
+    public void testGetFragAtPosition(){
+       	Story s = new Story();
+    	Fragment f = new Fragment();
+    	sc.addFragment(s, f);
+    	assert(s.getFragments().size() == 1);
+    	Fragment f2 = sc.getFragmentsPos(s, 0);
+    	assert(f.equals(f2));
+    }
+    
+    /**
+     * Tests setting a fragment at a certain position
+     */
+    public void testSetFragAtPosition(){
+       	Story s = new Story();
+    	Fragment f = new Fragment();
+    	f.setTitle("HIHI");
+    	sc.addFragment(s, f);
+    	assert(s.getFragments().get(0).getTitle().equals("HIHI"));
+    	Fragment f1 = new Fragment();
+    	f1.setTitle("HEY");
+    	sc.setFragmentAtLocation(s, 0, f1);
+    	assert(s.getFragments().get(0).getTitle().equals("HEY"));
+    }
 }
