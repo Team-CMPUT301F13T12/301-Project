e0d8487869eb08cdf20e585e7952128b041e34f9
vzphung <vzphung@ualberta.ca>
2013-11-21 22:53:56 -0700

    added and did quick changes to ES Demo lab files
    i dont think any of this works yet
    also needs to add comments like where we got it from an stuff

---
+++ src/ualberta/g12/adventurecreator/Hits.java
@@ -0,0 +1,16 @@
+package ualberta.g12.adventurecreator;
+
+import java.util.Collection;
+
+
+public class Hits<T> {
+    int total;
+    double max_score;
+    Collection<ElasticSearchResponse<T>> hits;
+    public Collection<ElasticSearchResponse<T>> getHits() {
+        return hits;
+    }
+    public String toString() {
+        return (super.toString()+","+total+","+max_score+","+hits);
+    }
+}
