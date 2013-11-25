1cf1df9fe17663581aa77715c2110b472301d5d8
kclau301 <kclau@ualberta.ca>
2013-11-25 12:54:03 -0700

    updated help

---
+++ src/ualberta/g12/adventurecreator/views/HelpOnline.java
@@ -0,0 +1,21 @@
+
+package ualberta.g12.adventurecreator.views;
+
+import ualberta.g12.adventurecreator.R;
+import ualberta.g12.adventurecreator.R.layout;
+import ualberta.g12.adventurecreator.R.menu;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class HelpOnline extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_online);
+    }
+
+
+}
