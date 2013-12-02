7180ec038e587adec173fed4bdebf72fe16d6bf0
LindsayL <livojevi@ualberta.ca>
2013-12-01 20:32:48 -0700

    Edited FController and updated docs for FragmentController and FController

--- src/ualberta/g12/adventurecreator/controllers/FController.java
+++ src/ualberta/g12/adventurecreator/controllers/FController.java
@@ -1,7 +1,9 @@
 
 package ualberta.g12.adventurecreator.controllers;
 
+import ualberta.g12.adventurecreator.data.Choice;
 import ualberta.g12.adventurecreator.data.Fragment;
+import ualberta.g12.adventurecreator.data.FragmentPart;
 
 /**
  * Control of data in the Fragment model. Controls fragment modifications within
@@ -11,31 +13,82 @@ import ualberta.g12.adventurecreator.data.Fragment;
 public interface FController {
 
     /**
-     * Sets the title of the given Fragment to the given String, which should
-     * not be null
+     * Changes the title of the given fragment to the given title. There are no
+     * limits on the length of this title.
      * 
-     * @param frag is the Fragment to be changed
-     * @param newTitle the new title of the fragment
+     * @param fragment the fragment whose title we are changing
+     * @param newTitle the new title of the fragment, must not be null
      */
-    public void setTitle(Fragment frag, String newTitle);
+    public void setTitle(Fragment fragment, String newTitle);
+    
+    /**
+     * Adds a new Fragment part of the given type to the FragmentPart
+     * list of the fragment.  
+     * 
+     * @param fragment to add the FragmentPart to
+     * @param type is the type of FragmentPart to add, should be "t" 
+     * (text), "i" (illustration), "c" (choice), or "e" (empty)
+     * @param pos is the position to insert the FragmentPart in the 
+     * FragmentPart list of fragment
+     * @return the newly created FragmentPart 
+     * @see FragmentPart
+     */
+    public FragmentPart addNewFragmentPart(Fragment fragment, String type, int pos);
 
     /**
-     * Removes an empty fragment part that was added to this Fragment. If none
-     * exist, nothing happens.
+     * Sets the data field of a FragmentPart in the FragmentPart 
+     * list of fragment.  Should only be used on "t" (text) or "i" 
+     * (illustration) type FragmentParts.
      * 
-     * @param frag is the Fragment to be changed
+     * @param fragment Fragment that owns the given FragmentPart, part
+     * @param part the FragmentPart to modify
+     * @param data the new data to be set for part, must not be null
+     * @see FragmentPart
      */
-    public void removeEmptyPart(Fragment frag);
+    public void setFragmentPartData(Fragment fragment, FragmentPart part, String data);
 
     /**
-     * Removes the FragmentPart specified by its partNum (it's number in the
-     * displayorder) from the fragment. The behavior is unspecified if the
-     * fragment doesn't exist or if there is no part at partNum
+     * Sets the choice field of a FragmentPart in the FragmentPart 
+     * list of fragment.  Should only be used on "c" (choice) type 
+     * FragmentParts.
      * 
-     * @param frag is the Fragment to be changed
-     * @param partNum the fragmentPart number of the fragmentPart in the display
-     *            order to be removed
+     * @param fragment Fragment that owns the given FragmentPart, part
+     * @param part the FragmentPart to modify
+     * @param choice the new choice to be set for part, must not be null
+     * @see FragmentPart
      */
-    public boolean deleteFragmentPart(Fragment frag, int partNum);
+    public void setFragmentPartChoice(Fragment fragment, FragmentPart part, Choice choice);
 
+    /**
+     * Sets the picSize field of a FragmentPart in the FragmentPart 
+     * list of fragment.  Should only be used on "i" (illustration) 
+     * type FragmentParts.
+     * 
+     * @param fragment Fragment that owns the given FragmentPart, part
+     * @param part the FragmentPart to modify
+     * @param picSize the new picSize to be set for part, must not be null
+     * @see FragmentPart
+     */
+    public void setFragmentPartPicSize(Fragment fragment, FragmentPart part, int picSize);
+    
+    /**
+     * Tries to remove a fragment part at the given partNum. If there is no part
+     * at this partNum, we will return false and perform no deletions. If there
+     * is a fragment part at this partNum we will delete it return true.
+     * 
+     * @param fragment the fragment to delete the choice from
+     * @param partNum the position of the fragment part to delete
+     * @return true if the choice was deleted, otherwise false
+     */
+    public boolean deleteFragmentPart(Fragment fragment, int partNum);
+
+    /**
+     * Removes the first occurrence of an empty FragmentPart, type = "e", 
+     * in the FragmentPart list of fragment. If there are no empty parts 
+     * nothing is deleted.
+     * 
+     * @param fragment the fragment to remove the empty part from.
+     * @see FragmentPart
+     */
+    public void removeEmptyPart(Fragment fragment);
 }
--- src/ualberta/g12/adventurecreator/controllers/FragmentController.java
+++ src/ualberta/g12/adventurecreator/controllers/FragmentController.java
@@ -13,46 +13,90 @@ import java.util.List;
  * {@link FController}.
  */
 public class FragmentController implements FController {
-
-    private static final String TAG = "FragmentController";
-
+    
     /**
      * Changes the title of the given fragment to the given title. There are no
      * limits on the length of this title.
      * 
-     * @param frag the fragment whose title we are changing
-     * @param newTitle the new title of the fragment
+     * @param fragment the fragment whose title we are changing
+     * @param newTitle the new title of the fragment, must not be null
      */
     @Override
-    public void setTitle(Fragment frag, String newTitle) {
-        frag.setTitle(newTitle);
+    public void setTitle(Fragment fragment, String newTitle) {
+        fragment.setTitle(newTitle);
     }
 
-    public FragmentPart addNewFragmentPart(Fragment frag, String type, int pos) {
+    /**
+     * Adds a new Fragment part of the given type to the FragmentPart
+     * list of the fragment.  
+     * 
+     * @param fragment to add the FragmentPart to
+     * @param type is the type of FragmentPart to add, should be "t" 
+     * (text), "i" (illustration), "c" (choice), or "e" (empty)
+     * @param pos is the position to insert the FragmentPart in the 
+     * FragmentPart list of fragment
+     * @return the newly created FragmentPart 
+     * @see FragmentPart
+     */
+    @Override
+    public FragmentPart addNewFragmentPart(Fragment fragment, String type, int pos) {
         FragmentPart newPart = new FragmentPart(type);
-        List<FragmentPart> parts = frag.getParts();
+        List<FragmentPart> parts = fragment.getParts();
         try {
             parts.add(pos, newPart);
         } catch (IndexOutOfBoundsException e) {
             return null;
         }
-        frag.setParts(parts);
+        fragment.setParts(parts);
         return newPart;
     }
 
-    public void setFragmentPartData(Fragment frag, FragmentPart part, String data) {
+    /**
+     * Sets the data field of a FragmentPart in the FragmentPart 
+     * list of fragment.  Should only be used on "t" (text) or "i" 
+     * (illustration) type FragmentParts.
+     * 
+     * @param fragment Fragment that owns the given FragmentPart, part
+     * @param part the FragmentPart to modify
+     * @param data the new data to be set for part, must not be null
+     * @see FragmentPart
+     */
+    @Override
+    public void setFragmentPartData(Fragment fragment, FragmentPart part, String data) {
         part.setData(data);
-        frag.setParts(frag.getParts());
+        fragment.setParts(fragment.getParts());
     }
 
-    public void setFragmentPartChoice(Fragment frag, FragmentPart part, Choice choice) {
+    /**
+     * Sets the choice field of a FragmentPart in the FragmentPart 
+     * list of fragment.  Should only be used on "c" (choice) type 
+     * FragmentParts.
+     * 
+     * @param fragment Fragment that owns the given FragmentPart, part
+     * @param part the FragmentPart to modify
+     * @param choice the new choice to be set for part, must not be null
+     * @see FragmentPart
+     */
+    @Override
+    public void setFragmentPartChoice(Fragment fragment, FragmentPart part, Choice choice) {
         part.setChoice(choice);
-        frag.setParts(frag.getParts());
+        fragment.setParts(fragment.getParts());
     }
 
-    public void setFragmentPartPicSize(Fragment frag, FragmentPart part, int picSize) {
+    /**
+     * Sets the picSize field of a FragmentPart in the FragmentPart 
+     * list of fragment.  Should only be used on "i" (illustration) 
+     * type FragmentParts.
+     * 
+     * @param fragment Fragment that owns the given FragmentPart, part
+     * @param part the FragmentPart to modify
+     * @param picSize the new picSize to be set for part, must not be null
+     * @see FragmentPart
+     */
+    @Override
+    public void setFragmentPartPicSize(Fragment fragment, FragmentPart part, int picSize) {
         part.setPicSize(picSize);
-        frag.setParts(frag.getParts());
+        fragment.setParts(fragment.getParts());
     }
 
     /**
@@ -60,62 +104,38 @@ public class FragmentController implements FController {
      * at this partNum, we will return false and perform no deletions. If there
      * is a fragment part at this partNum we will delete it return true.
      * 
-     * @param frag the fragment to delete the choice from
+     * @param fragment the fragment to delete the choice from
      * @param partNum the position of the fragment part to delete
      * @return true if the choice was deleted, otherwise false
      */
     @Override
-    public boolean deleteFragmentPart(Fragment frag, int partNum) {
-        List<FragmentPart> parts = frag.getParts();
+    public boolean deleteFragmentPart(Fragment fragment, int partNum) {
+        List<FragmentPart> parts = fragment.getParts();
         try {
             parts.remove(partNum);
         } catch (IndexOutOfBoundsException e) {
             return false;
         }
-        frag.setParts(parts);
+        fragment.setParts(parts);
         return true;
     }
 
     /**
-     * Removes the first occurrence of an empty part in the Fragments list of
-     * segments. If there are no empty parts, nothing is deleted.
+     * Removes the first occurrence of an empty FragmentPart, type = "e", 
+     * in the FragmentPart list of fragment. If there are no empty parts 
+     * nothing is deleted.
      * 
-     * @param frag the fragment to remove the empty part from.
+     * @param fragment the fragment to remove the empty part from.
+     * @see FragmentPart
      */
     @Override
-    public void removeEmptyPart(Fragment frag) {
-        List<FragmentPart> parts = frag.getParts();
+    public void removeEmptyPart(Fragment fragment) {
+        List<FragmentPart> parts = fragment.getParts();
         for (int i = 0; i < parts.size(); i++) {
             if (parts.get(i).getType().equals("e")) {
-                deleteFragmentPart(frag, i);
+                deleteFragmentPart(fragment, i);
                 break;
             }
-        }
-    }
-
-    /**
-     * Retrieve the fragment that a particular choice is linked to
-     * 
-     * @param frag is the fragment that the choice is contained in
-     * @param partNum is the position of the choice in the fragmentParts
-     * @return Fragment pointed to by choice.  If it is not a choice
-     * at the displayOrderPos returns null.  
-     */
-    public int getLinkedToFragmentPosOfChoice(Fragment frag, int partNum) {
-        List<FragmentPart> parts = frag.getParts();
-        FragmentPart part;
-        try{
-            part = parts.get(partNum);
-        } catch (IndexOutOfBoundsException e){
-            return -1;
-        }
-        
-        if (!(part.getType().equals("c")))
-            return -1;
-        else {
-            //safe to cast now
-            Choice choice = part.getChoice();
-            return choice.getLinkedToFragmentPos();
         }
     }
 }
