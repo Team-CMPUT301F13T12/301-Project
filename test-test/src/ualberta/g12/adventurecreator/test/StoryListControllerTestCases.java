e14b54ce0717d0ca62afc2956166a473354e0061
kclau301 <kclau@ualberta.ca>
2013-12-02 02:31:03 -0700

    more docs

--- test-test/src/ualberta/g12/adventurecreator/test/StoryListControllerTestCases.java
+++ test-test/src/ualberta/g12/adventurecreator/test/StoryListControllerTestCases.java
@@ -8,12 +8,20 @@ import junit.framework.TestCase;
 import ualberta.g12.adventurecreator.controllers.FragmentController;
 import ualberta.g12.adventurecreator.controllers.StoryListController;
 import ualberta.g12.adventurecreator.data.Fragment;
+import ualberta.g12.adventurecreator.data.OfflineIOHelper;
 import ualberta.g12.adventurecreator.data.Story;
 import ualberta.g12.adventurecreator.data.StoryList;
 import ualberta.g12.adventurecreator.views.FragmentEditActivity;
 import ualberta.g12.adventurecreator.views.MainActivity;
 
 import java.util.List;
+
+/**
+ * These test cases test that the StoryList controller behave to meet the needed requirements.
+ * Function that will be tested are: addStory, setStory, and createBlankStory().
+ * The storyList controller is made of getteres and setteres that control all variables
+ * From the storyList.
+ */
 
 public class StoryListControllerTestCases extends ActivityInstrumentationTestCase2<MainActivity> {
     private FragmentController fc;
@@ -43,6 +51,9 @@ public class StoryListControllerTestCases extends ActivityInstrumentationTestCas
         assertNotNull("StoryListController was null", sl);
     }
 
+    /**
+     * Creates a new story to the story list
+     */
     public void testAddStory() {
 
         // add a story to our storyList
@@ -57,6 +68,9 @@ public class StoryListControllerTestCases extends ActivityInstrumentationTestCas
     }
 
 
+    /**
+     * tests the getStoryAtPos function to test if story details can be retrieved 
+     */
     public void testGetStoryWithObject() {
 
         // add a story to our storyList
@@ -69,6 +83,9 @@ public class StoryListControllerTestCases extends ActivityInstrumentationTestCas
         assertTrue("Stories don't have same authors", s.getAuthor().equals(s2.getAuthor()));
     }
 
+    /**
+     * tests the getTitle function, to see if a title is received
+     */
     public void testGetStoryWithTitle() {
 
         // add a story to our storyList
@@ -82,7 +99,9 @@ public class StoryListControllerTestCases extends ActivityInstrumentationTestCas
         assertTrue("Stories don't have same authors", s.getAuthor().equals(s3));
     }
 
-    // tests setting a story
+    /**
+     * tests to see whether a story set in a new position takes its place in the story list
+     */
     public void testSetStory() {
 
         // add a story to our storyList
@@ -98,7 +117,10 @@ public class StoryListControllerTestCases extends ActivityInstrumentationTestCas
 
     }
 
-    // testing getting a story at Position
+    /**
+     * tests the getStoryAtPos function which should return a certain story from a 
+     * specific position in the storylist
+     */
     public void testGetStoryAtPos() {
         // add a story to our storyList
         sl = new StoryList();
@@ -112,7 +134,9 @@ public class StoryListControllerTestCases extends ActivityInstrumentationTestCas
 
     }
 
-    // testing getting a story at Position
+    /**
+     * Tests whether a blank story (story with no parameters) can be made
+     */
     public void testCreateBlankStory() {
 
         // add a story to our storyList
@@ -124,6 +148,9 @@ public class StoryListControllerTestCases extends ActivityInstrumentationTestCas
 
     }
 
+    /**
+     * test if the initialized screen with no stories does in fact return no stories
+     */
     public void testGetInitialListOfStories() {
         sl = new StoryList();
         slc = new StoryListController(sl, null);
@@ -133,6 +160,9 @@ public class StoryListControllerTestCases extends ActivityInstrumentationTestCas
         assertTrue("Initial list of stories wasn't 0", sl.getAllStories().size() == 0);
     }
 
+    /**
+     * tests if new stories can be added to the story list with the addStory function 
+     **/
     public void testAddStoriesToList() {
         Story s = new Story("A Dance With Dragons", "George R.R. Martin");
         int oldSize = sl.getAllStories().size();
@@ -148,6 +178,9 @@ public class StoryListControllerTestCases extends ActivityInstrumentationTestCas
                 oldSize < stories.size());
     }
 
+    /**
+     * tests if by using the setStory function, more stories can be added to the storylist
+     */
     public void testSetStory2() {
         Story s = new Story("The Winds of Winter", "George R.R. Martin");
         Story s1 = new Story("A Storm of Swords", "George R.R. Martin");
