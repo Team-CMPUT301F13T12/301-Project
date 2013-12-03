eef54da177922b9e83637aa1ac6f4f474b482bee
tophernuts <tophernuts@gmail.com>
2013-12-02 21:50:48 -0700

    Added publish and then download test case

--- test-test/src/ualberta/g12/adventurecreator/test/PublishTaskTestCases.java
+++ test-test/src/ualberta/g12/adventurecreator/test/PublishTaskTestCases.java
@@ -2,12 +2,17 @@
 package ualberta.g12.adventurecreator.test;
 
 import android.content.Context;
+import android.os.AsyncTask.Status;
 import android.test.ActivityInstrumentationTestCase2;
 
+import ualberta.g12.adventurecreator.data.AdventureCreator;
 import ualberta.g12.adventurecreator.data.Story;
+import ualberta.g12.adventurecreator.tasks.DownloadStoryTask;
 import ualberta.g12.adventurecreator.tasks.PublishStoryTask;
 import ualberta.g12.adventurecreator.tasks.TryPublishStoryTask;
 import ualberta.g12.adventurecreator.views.MainActivity;
+
+import java.util.List;
 
 /**
  * These test cases test the tasks of the online to offline and vice versa
@@ -22,6 +27,7 @@ public class PublishTaskTestCases extends
 
     private PublishStoryTask publishTask;
     private TryPublishStoryTask tryPublishTask;
+    private DownloadStoryTask downloadTask;
     private Context context;
 
     public PublishTaskTestCases() {
@@ -72,17 +78,93 @@ public class PublishTaskTestCases extends
             @Override
             public void run() {
                 publishTask.execute(new Story[] {
-                    s
+                        s
                 });
             }
         });
-        
-        if(this.publishTask.get()){
+
+        if (this.publishTask.get()) {
             // Upload worked
-            
+
         } else {
             // Upload failed
             fail("Publish failed");
         }
+    }
+
+    public void testPublishDownload() throws Throwable {
+        this.publishTask = new PublishStoryTask(this.context);
+        this.downloadTask = new DownloadStoryTask(this.context);
+
+        final Story s = new Story("AB", "AB");
+
+        runTestOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+                publishTask.execute(new Story[] {
+                        s
+                });
+            }
+        });
+        if (!this.publishTask.get()) {
+            // Upload failed
+            fail("publish failed");
+        }
+
+        while (publishTask.getStatus() != Status.FINISHED) {
+            if (publishTask.isCancelled()) {
+                fail("Publish task was cancelled");
+            }
+        }
+
+        List<Story> list = AdventureCreator.getStoryList().getAllStories();
+        int count = 0;
+        for (Story d : list) {
+            if (d.getTitle().equals("AB") && d.getAuthor().equals("AB")) {
+                count++;
+            }
+        }
+
+        final int finalCount = count;
+
+        runTestOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+                downloadTask.execute(new Story[] {
+                        s
+                });
+            }
+        });
+
+        // Wait till we'ere done
+        if (downloadTask.get().contains("Download failed")) {
+            // Download Failed
+            fail("Download failed");
+        }
+
+        // Wait til onPostExecute is done
+        while (downloadTask.getStatus() != Status.FINISHED) {
+            if (downloadTask.isCancelled()) {
+                fail("We were cancelled");
+            }
+        }
+
+        runTestOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+                List<Story> sl = AdventureCreator.getStoryList().getAllStories();
+                int otherCount = 0;
+                for (Story r : sl) {
+                    if (r.getAuthor().equals("AB") && r.getTitle().equals("AB")) {
+                        otherCount++;
+                    }
+                }
+                assertTrue("There wasn't an additional AB story after downloading it",
+                        otherCount > finalCount);
+            }
+        });
     }
 }
